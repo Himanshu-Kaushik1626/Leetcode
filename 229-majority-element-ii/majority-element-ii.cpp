a403e8228d1789c596f80489f772c3adfa838f13
Himanshu Sharma <150991635+Himanshu-Kaushik1626@users.noreply.github.com>
2026-09-03 10:14:47 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (49.54%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n= nums.size();
-        map<int, int> mp;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        vector<int> ans;
-        for(auto s: mp){
-            if(s.second > n/3){
-                ans.push_back(s.first);
+        int el1=INT_MIN,el2=INT_MIN;
+        int cnt1=0,cnt2=0;
+        for(auto it:nums){
+            if(cnt1==0 && el2!=it){
+                el1=it;
+                cnt1=1;
+            }
+            else if(cnt2==0 && el1!=it){
+                el2=it;
+                cnt2=1;
+            }
+            else if(it==el1) cnt1++;
+            else if(it==el2) cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
             }
         }
+        cnt1=0,cnt2=0;
+        for(auto it:nums){
+            if(it==el1) cnt1++;
+            else if(it==el2) cnt2++;
+        }
+        vector<int> ans;
+        if(cnt1>nums.size()/3) ans.push_back(el1);
+        if(cnt2>nums.size()/3) ans.push_back(el2);
         return ans;
     }
 };
